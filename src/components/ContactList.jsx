49daf5a33d2bd7bc71862ab73271ff7c5a72b3e8
hnejman <108231655+hnejman@users.noreply.github.com>
2023-10-04 15:08:17 +0200

    end

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -8,30 +8,36 @@ export const ContactList = ({ contacts, searchBy, deleteItem }) => {
     );
   });
 
-if (elements.length) {
-  return (
-    <ul>
-      {elements.map(ele => {
-        return (
-          <li key={ele.id} id={ele.id}>
-            {ele.name + ': ' + ele.number}
-            <button
-              onClick={e => {
-                deleteItem(e);
-              }}
-            >
-              delete
-            </button>
-          </li>
-        )
-      })}
-    </ul>
-  )
-}
+  if (elements.length) {
+    return (
+      <ul>
+        {elements.map(ele => {
+          return (
+            <li key={ele.id} id={ele.id}>
+              {ele.name + ': ' + ele.number}
+              <button
+                onClick={e => {
+                  deleteItem(e);
+                }}
+              >
+                delete
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+    );
+  }
 };
 
 ContactList.propTypes = {
-  contacts: PropTypes.array,
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+      id: PropTypes.string.isRequired
+    })
+  ),
   searchBy: PropTypes.string,
-  deleteItem: PropTypes.func.isRequired
-}
+  deleteItem: PropTypes.func.isRequired,
+};
